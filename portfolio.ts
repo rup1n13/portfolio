3a2f17009bb4bc8b33d17c7003a626c067a25e89
rup1n13 <adjohouesyle@gmail.com>
2025-06-30 11:03:04 +0100

    update resume link

--- portfolio.ts
+++ portfolio.ts
@@ -16,7 +16,7 @@ export const greetings: GreetingsType = {
   title: "Hello world 👋, Je suis Rupinie Elysée ADJOHOU",
   description:
     "Je suis un développeur web et mobile passionné qui s'efforce de devenir Full Stack, avec une forte motivation à apprendre et à m'améliorer constamment. J'explore de nouvelles technologies et cherche à les intégrer dans mes projets. Curieux et avide de connaissances, je m'essaie également à la cybersécurité et aux CTF pour diversifier mes compétences. Travailleur, autonome et toujours prêt à apprendre des autres, je vise à m'épanouir dans le développement et à repousser mes limites.",
-  resumeLink: "https://drive.google.com/file/d/1Ol9RyDGRvcnSvqChKhiLvo-CVYPC-bXe/view?usp=sharing",
+  resumeLink: "https://drive.google.com/file/d/1grigeEp_qSmo2-7iVbeSOBcSrRegoaHi/view?usp=drive_link",
 };
 
 export const openSource = {
